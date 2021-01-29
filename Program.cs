791ebcaf60dd370916f5709b29b7dcb095ae44d5
LC <llebpmacl1@gmail.com>
2021-01-29 12:13:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 100");
+
+            int entry = 0;
+            string input = Console.ReadLine();
+            int user_input = int.Parse(input);
+            while (entry < user_input)
+            {
+                entry += 1;
+                Console.WriteLine("You have entered " + user_input + ". The current integer value in the loop:" + entry);
+            }
+
+            
+
+            
+
+        }
+    }
+}
